f9900927d074980b72cab593de15f42d50287e9c
tracer-ics2019 <tracer@njuics.org>
2019-09-09 19:32:20 +0800

    >  compile 181240019 bright Linux Debian10 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  19:32:20 up  6:12,  1 user,  load average: 0.02, 0.01, 0.00 d0785aa4ad238555210df152dfbe56a34ff03abe

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -3,7 +3,7 @@
 #include "monitor/watchpoint.h"
 #include "nemu.h"
 #include "cpu/exec.h"
-#include "../../isa/riscv32/reg.c"
+#include "reg.c"
 
 #include <stdlib.h>
 #include <readline/readline.h>
