fad75e6d54cb96153feeca76f26ec7b03b131317
181240019-Hong Liang <181240019@smail.nju.edu.cn>
2019-09-21 14:15:27 +0800

    finished on expression generating, but still have to add random spaces to it and link it to expr()

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -7,8 +7,43 @@
 
 // this should be enough
 static char buf[65536];
+
+static int idx;
+
+uint32_t choose(uint32_t n){
+	uint32_t ran = rand()%n;
+	return ran;
+}
+
+static inline void gen_num(){
+	int nu = rand()%10;
+	char pu = nu + '0';
+	buf[idx] = pu;
+	idx++;
+}
+
+static inline void gen_rand_op(){
+	int nu = random()%4;
+	char lst[] = {'+','-','*','/'};
+	char pu = lst[nu];
+	buf[idx] = pu;
+	idx++;
+}
+
+static inline void gen(char sig){
+	buf[idx] = sig;
+	idx++;
+}
+
 static inline void gen_rand_expr() {
-  buf[0] = '\0';
+//  buf[0] = '\0';
+  idx = 0;
+  switch(choose(3)){
+	case 0 : gen_num; break;
+	case 1 : gen('('); gen_rand_expr(); gen(')'); break;
+	
+	default : gen_rand_expr(); gen_rand_op(); gen_rand_expr(); break;
+  }
 }
 
 static char code_buf[65536];
@@ -30,7 +65,8 @@ int main(int argc, char *argv[]) {
   int i;
   for (i = 0; i < loop; i ++) {
     gen_rand_expr();
-
+/*  add an end here  */	
+    buf[idx] = '\0';
     sprintf(code_buf, code_format, buf);
 
     FILE *fp = fopen("/tmp/.code.c", "w");
