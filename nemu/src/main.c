a4ff61ea48223ffda3edc2774443e23e18027344
tracer-ics2019 <tracer@njuics.org>
2019-09-22 11:01:38 +0800

    >  compile 181240019 bright Linux Debian10 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  11:01:38 up  3:05,  1 user,  load average: 0.07, 0.03, 0.01 5b76279a271e2309685643607f0e24d8f73317ea

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -34,6 +34,7 @@ int main(int argc, char *argv[]) {
 	memset(line, 0, sizeof(line));
 	memset(exp, 0, sizeof(exp));
 	memset(val, 0, sizeof(val));
+	printf("shit\n");
 	fgets(line, 65536, cp);
 	printf("%s\n",line);
 	while(line[p]!=' '){
