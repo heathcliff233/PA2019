50b447ac0898e9e34089f3926a9f7f2e6cea4fd5
tracer-ics2019 <tracer@njuics.org>
2019-10-13 19:39:00 +0800

    >  compile 181240019 bright Linux Debian10 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  19:39:00 up 1 day,  7:36,  1 user,  load average: 0.03, 0.05, 0.01 5e805a9387913c2c3d01f8c15e1ae2ee1710f32b

--- nemu/src/isa/riscv32/exec/compute.c
+++ nemu/src/isa/riscv32/exec/compute.c
@@ -23,7 +23,7 @@ make_EHelper(jal){
   //rtl_addi(&id_dest->reg, &cpu.pc, 4);
   //rtl_j(id_src->val);
   reg_l(id_dest->reg)=cpu.pc+4;
-  cpu.pc=cpu.pc+0xc;
+  cpu.pc=cpu.pc+((int)id_src->val>>25);
 }
 
 make_EHelper(add){
