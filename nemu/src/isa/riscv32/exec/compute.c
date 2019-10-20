7b8aff2931ce62cdf05c396f5f5097baf2394de3
tracer-ics2019 <tracer@njuics.org>
2019-10-20 12:14:31 +0800

    >  compile 181240019 bright Linux Debian10 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  12:14:31 up 21:48,  1 user,  load average: 0.08, 0.02, 0.00 ced3168956bf6f5cb56e2224a8fa81662fa2901

--- nemu/src/isa/riscv32/exec/compute.c
+++ nemu/src/isa/riscv32/exec/compute.c
@@ -65,7 +65,7 @@ make_EHelper(ec){
 }
 
 make_EHelper(add){
-  printf("%x,%x\n",reg_l(id_src->reg),reg_l(id_src2->reg));
+  printf("%x,%x,%d\n",reg_l(id_src->reg),reg_l(id_src2->reg),decinfo.isa.instr.funct7);
   switch((int)decinfo.isa.instr.funct7 ){
 	  case 0 : rtl_add(&reg_l(id_dest->reg),&reg_l(id_src->reg),&reg_l(id_src2->reg));
 	  case 1 : rtl_mul_lo(&reg_l(id_dest->reg),&reg_l(id_src->reg),&reg_l(id_src2->reg));
