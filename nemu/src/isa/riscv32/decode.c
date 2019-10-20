1a9cfdf899ec30e2cbe251bd8a0989ce805ba7a8
tracer-ics2019 <tracer@njuics.org>
2019-10-21 03:20:14 +0800

    >  compile 181240019 bright Linux Debian10 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  03:20:14 up 1 day,  7:34,  1 user,  load average: 0.10, 0.07, 0.02 8e379ee738ea966d889240e4a21d9e2ea1a2bcc1

--- nemu/src/isa/riscv32/decode.c
+++ nemu/src/isa/riscv32/decode.c
@@ -70,10 +70,10 @@ make_DHelper(JR) {
 }
 
 make_DHelper(BQ) {
-   decode_op_i(id_dest, (decinfo.isa.instr.imm4_1<<1)+(decinfo.isa.instr.simm11_5<<5)+(decinfo.isa.instr.simm12<<12)+(decinfo.isa.instr.imm11<<11),true);
+   decode_op_i(id_dest, (decinfo.isa.instr.imm4_1<<1)+(decinfo.isa.instr.imm10_5<<5)+(decinfo.isa.instr.simm12<<12)+(decinfo.isa.instr.imm11<<11),true);
   decode_op_r(id_src, decinfo.isa.instr.rs1, true);
   decode_op_r(id_src2,decinfo.isa.instr.rs2, true);
-  id_dest->val = ((int32_t)id_dest->val)<<20>>20;
+  //id_dest->val = ((int32_t)id_dest->val)<<20>>20;
 }   
 
 make_DHelper(EC) {
