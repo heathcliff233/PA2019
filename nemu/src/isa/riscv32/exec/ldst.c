58c25634473c1489d6e1ec947e31636f6391ab01
tracer-ics2019 <tracer@njuics.org>
2019-10-20 13:00:34 +0800

    >  compile 181240019 bright Linux Debian10 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  13:00:34 up 22:34,  1 user,  load average: 0.27, 0.09, 0.03 86ffe97b6a2b57a68ca6abcd7a35fec90a46a35

--- nemu/src/isa/riscv32/exec/ldst.c
+++ nemu/src/isa/riscv32/exec/ldst.c
@@ -1,8 +1,12 @@
 #include "cpu/exec.h"
 
 make_EHelper(ld) {
-  rtl_lm(&s0, &id_src->addr, decinfo.width);
+/*  rtl_lm(&s0, &id_src->addr, decinfo.width);
   rtl_sr(id_dest->reg, &s0, 4);
+*/  
+  rtl_lm(&id_dest->reg, &id_src->addr, decinfo.width);
+  rtl_sr(id_dest->reg, &id_dest->reg, 4);
+
 
   switch (decinfo.width) {
     case 4: print_asm_template2(lw); break;
