52ae8195e01a39905e0b8f4d3a0727917566821e
tracer-ics2019 <tracer@njuics.org>
2019-10-23 22:26:12 +0800

    >  compile 181240019 bright Linux Debian10 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  22:26:12 up  4:23,  1 user,  load average: 0.10, 0.03, 0.01 20feaade6e3dc79970a81697b786498cbdcff2c6

--- nemu/src/isa/riscv32/diff-test.c
+++ nemu/src/isa/riscv32/diff-test.c
@@ -2,16 +2,16 @@
 #include "monitor/diff-test.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  ref_difftest_getregs(&ref_r);
+  ref_difftest_getregs(ref_r);
   int i = 0;
   for(i=0;i<32;i++){
-    if(cpu.gpr[i]._32 != ref_r->gpr[i]._32){
-	  printf("nemu get gpr[%4d]=0x%x while it should be 0x%x",i,cpu.gpr[i]._32,ref_r->gpr[i]._32);
+    if(cpu.gpr[i]._32 != (*ref_r).gpr[i]._32){
+	  printf("nemu get gpr[%4d]=0x%x while it should be 0x%x",i,cpu.gpr[i]._32,(*ref_r).gpr[i]._32);
 	  return false;
 	}
   }
-  if(cpu.pc != ref_r->pc){
-	printf("nemu pc points to 0x%x while it should be at 0x%x",cpu.pc,ref_r->pc);
+  if(cpu.pc != (*ref_r).pc){
+	printf("nemu pc points to 0x%x while it should be at 0x%x",cpu.pc,(*ref_r).pc);
 	return false;
   }
   return true;
