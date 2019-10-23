15bcec6b82ccab780a77bf633b08f0d69bb7ff4b
tracer-ics2019 <tracer@njuics.org>
2019-10-23 22:51:25 +0800

    >  compile 181240019 bright Linux Debian10 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  22:51:25 up  4:48,  1 user,  load average: 0.04, 0.05, 0.01 268a26548b469df3ca71e8ee9bea4ffeba3dd5ae

--- nemu/src/isa/riscv32/diff-test.c
+++ nemu/src/isa/riscv32/diff-test.c
@@ -4,17 +4,18 @@
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   ref_difftest_getregs(ref_r);
   int i = 0;
+  bool ret = true;
   for(i=0;i<32;i++){
     if(cpu.gpr[i]._32 != ref_r->gpr[i]._32){
 	  printf("nemu get gpr[%4d]=0x%x while it should be 0x%x",i,cpu.gpr[i]._32,ref_r->gpr[i]._32);
-	  return false;
+	  ret = false;
 	}
   }
   if(cpu.pc != ref_r->pc){
 	printf("nemu pc points to 0x%x while it should be at 0x%x",cpu.pc,(*ref_r).pc);
-	return false;
+	ret = false;
   }
-  return true;
+  return ret;
 }
 
 void isa_difftest_attach(void) {
