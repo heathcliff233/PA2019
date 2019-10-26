511aaaeed5a9e69b75e8ad0d108c9e79aaefd594
tracer-ics2019 <tracer@njuics.org>
2019-10-26 23:20:31 +0800

    >  run 181240019 bright Linux Debian10 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  23:20:31 up  4:47,  1 user,  load average: 0.01, 0.07, 0.04 e9b0a31d843562ac8ff8ba7541db0d4ad4ebddaf

--- nexus-am/am/src/nemu-common/nemu-input.c
+++ nexus-am/am/src/nemu-common/nemu-input.c
@@ -8,8 +8,9 @@ size_t __am_input_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_INPUT_KBD: {
       _DEV_INPUT_KBD_t *kbd = (_DEV_INPUT_KBD_t *)buf;
-      kbd->keydown = 0;
-      kbd->keycode = _KEY_NONE;
+      uint16_t tmp = inw(KBD_ADDR); 
+	  kbd->keydown = tmp & 0x8000 ?1:0;
+      kbd->keycode = tmp | 0x8000;
       return sizeof(_DEV_INPUT_KBD_t);
     }
   }
