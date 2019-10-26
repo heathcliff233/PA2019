aa02e03c78837a750a8383df99977344ac38f561
tracer-ics2019 <tracer@njuics.org>
2019-10-26 22:37:57 +0800

    >  run 181240019 bright Linux Debian10 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  22:37:57 up  4:05,  1 user,  load average: 0.10, 0.11, 0.09 f82dc8b2a1b3494f85d17ff3484cfdfc3702f5cc

--- nexus-am/am/src/nemu-common/nemu-timer.c
+++ nexus-am/am/src/nemu-common/nemu-timer.c
@@ -2,8 +2,6 @@
 #include <amdev.h>
 #include <nemu.h>
 
-#define RTC_PORT 0xa1000048
-
 static _DEV_TIMER_UPTIME_t boot_time;
 
 size_t __am_timer_read(uintptr_t reg, void *buf, size_t size) {
@@ -12,7 +10,7 @@ size_t __am_timer_read(uintptr_t reg, void *buf, size_t size) {
       _DEV_TIMER_UPTIME_t *uptime = (_DEV_TIMER_UPTIME_t *)buf;
       static uint32_t last_hi=0,last_lo=0;
 	  //uptime->hi = 0;
-      uptime->lo = inl(RTC_PORT);
+      uptime->lo = inl(RTC_ADDR);
       if(uptime->lo < last_lo) ++last_hi;
 	  last_lo = uptime->lo;
 	  uptime->hi = last_hi;
