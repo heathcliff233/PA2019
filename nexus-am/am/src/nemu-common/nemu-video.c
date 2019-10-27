4286a373d12592476176994a67007869a8cb256f
tracer-ics2019 <tracer@njuics.org>
2019-10-27 08:46:21 +0800

    >  run 181240019 bright Linux Debian10 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  08:46:21 up  6:00,  1 user,  load average: 0.08, 0.02, 0.01 41e8881b3801831ed484f757aa53424f3642925d

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -2,12 +2,15 @@
 #include <amdev.h>
 #include <nemu.h>
 
+#include <klib.h>
+
 size_t __am_video_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_INFO: {
       _DEV_VIDEO_INFO_t *info = (_DEV_VIDEO_INFO_t *)buf;
-      info->width = 0;
-      info->height = 0;
+      info->width = inl(SCREEN_ADDR);
+      info->height = info->width & 0xffff;
+	  info->width = info->width >> 16;
       return sizeof(_DEV_VIDEO_INFO_t);
     }
   }
@@ -18,7 +21,6 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_FBCTL: {
       _DEV_VIDEO_FBCTL_t *ctl = (_DEV_VIDEO_FBCTL_t *)buf;
-
       if (ctl->sync) {
         outl(SYNC_ADDR, 0);
       }
@@ -29,4 +31,10 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
 }
 
 void __am_vga_init() {
+  int i;
+  int size = screen_width()*screen_height();
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for(i=0;i<size;i++)fb[i]=i;
+  draw_sync();
+
 }
