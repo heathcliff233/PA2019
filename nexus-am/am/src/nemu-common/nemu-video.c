cf1908055f14b2977664624fc2f6ab3962f3890b
tracer-ics2019 <tracer@njuics.org>
2019-10-29 18:59:51 +0800

    >  run 181240019 bright Linux Debian10 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  18:59:51 up 37 min,  1 user,  load average: 0.26, 0.17, 0.08 15b5f6229be3b6f9c18461f5e300e44930aa60fc

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -35,10 +35,9 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
 		pixels += w;
 	  }
 	  if (ctl->sync) {
-        //outl(SYNC_ADDR, 0);
+        outl(SYNC_ADDR, 0);
       }
-      //return size;
-	  return sizeof(_DEV_VIDEO_FBCTL_t);
+      return size;
     }
   }
   return 0;
