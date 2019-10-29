7623a7044868a7d61f7ecdae9a106b693e2701ca
tracer-ics2019 <tracer@njuics.org>
2019-10-29 18:41:21 +0800

    >  run 181240019 bright Linux Debian10 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  18:41:21 up 19 min,  1 user,  load average: 0.00, 0.02, 0.05 c89fa6206c748c3eed39f946106ae63b1d74dbb0

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -23,17 +23,17 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_FBCTL: {
       _DEV_VIDEO_FBCTL_t *ctl = (_DEV_VIDEO_FBCTL_t *)buf;
-      int x=ctl->x,y=ctl->y,w=ctl->w,h=ctl->h;
-	  uint32_t *pixels = ctl->pixels;
+      //int x=ctl->x,y=ctl->y,w=ctl->w,h=ctl->h;
+	  //uint32_t *pixels = ctl->pixels;
+	  int h=ctl->h;
 	  for(int j=0; j<h; j++){
-	    memcpy(&fb[(y+j)*400+x],pixels,w*sizeof(uint32_t));
-		pixels += w;
+	    //memcpy(&fb[(y+j)*400+x],pixels,w*sizeof(uint32_t));
+		//pixels += w;
 	  }
 	  if (ctl->sync) {
-//        outl(SYNC_ADDR, 0);
+        outl(SYNC_ADDR, 0);
       }
-      //return size;
-	  return sizeof(_DEV_VIDEO_FBCTL_t);
+      return size;
     }
   }
   return 0;
