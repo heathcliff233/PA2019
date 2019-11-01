4d4ddc9901fbf1a807034e64ca32e3195ada342e
tracer-ics2019 <tracer@njuics.org>
2019-11-01 22:42:30 +0800

    >  run 181240030 bright Linux Debian10 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  22:42:30 up 26 min,  1 user,  load average: 0.04, 0.05, 0.06 ee6f37e4f86662ae36947a02d3112f28680c67

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -6,8 +6,9 @@ size_t __am_video_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_INFO: {
       _DEV_VIDEO_INFO_t *info = (_DEV_VIDEO_INFO_t *)buf;
-      info->width = 0;
-      info->height = 0;
+      info->width = inl(SCREEN_ADDR);
+      info->height = info->width & 0xffff;
+	  info->width = info->width >> 16;
       return sizeof(_DEV_VIDEO_INFO_t);
     }
   }
@@ -18,7 +19,14 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_FBCTL: {
       _DEV_VIDEO_FBCTL_t *ctl = (_DEV_VIDEO_FBCTL_t *)buf;
-
+	  int W = 300*400;
+	  uint32_t *fb=(uint32_t*)(uintptr_t)FB_ADDR;
+	  uint32_t *base = fb +ctl->y*W +ctl->x;
+	  for(int dy=0; dy < ctl->h; ++dy){
+		for(int dx=0; dx<ctl->w; ++dx){
+			base[dy*W+dx]=ctl->pixels[dy*ctl->w+dx];
+		}
+	  }
       if (ctl->sync) {
         outl(SYNC_ADDR, 0);
       }
