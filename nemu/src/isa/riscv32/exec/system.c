8c910b33ed2bbc58c827e5cf0be496517bf27cb7
tracer-ics2019 <tracer@njuics.org>
2019-11-17 21:22:18 +0800

    >  compile 181240019 bright Linux Debian10 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  21:22:18 up 15:26,  1 user,  load average: 0.03, 0.08, 0.04 8dea4907fcece9955b89f244c07c51a4ba6e0b0d

--- nemu/src/isa/riscv32/exec/system.c
+++ nemu/src/isa/riscv32/exec/system.c
@@ -65,11 +65,11 @@ make_EHelper(csrrw){
   uint32_t t;
   switch(id_src2->val){
        case 0x142: t = cpu.scause; 
-				   cpu.scause = 0;//reg_l(id_src->reg);
+				   cpu.scause = reg_l(id_src->reg);
 				   reg_l(id_dest->reg) = t;
 				   break;
        case 0x100: t = cpu.sstatus;
-				   cpu.sstatus = 0;//reg_l(id_src->reg);
+				   cpu.sstatus = reg_l(id_src->reg);
 				   reg_l(id_dest->reg) = t;
 				   break;
        case 0x141: t = cpu.sepc; 
@@ -77,7 +77,7 @@ make_EHelper(csrrw){
 				   reg_l(id_dest->reg) = t;
 				   break;
        case 0x105: t = cpu.stvec; 
-				   cpu.stvec = reg_l(id_src->reg);
+				   cpu.stvec = 0x80100004;//reg_l(id_src->reg);
 				   reg_l(id_dest->reg) = t;
 				   break;
        case 0x0  : break;
