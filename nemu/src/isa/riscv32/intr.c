2e0e023b7777444b83df15daedf81cc6462ef691
tracer-ics2019 <tracer@njuics.org>
2019-11-18 22:01:20 +0800

    >  compile 181240019 bright Linux Debian10 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  22:01:20 up 21:17,  1 user,  load average: 0.11, 0.09, 0.05 623d165cd5e2c898796cb5d38434199bf53b1fed

--- nemu/src/isa/riscv32/intr.c
+++ nemu/src/isa/riscv32/intr.c
@@ -7,8 +7,8 @@ void raise_intr(uint32_t NO, vaddr_t epc) {
   rtl_li(&cpu.sepc, epc);
   rtl_li(&cpu.scause, NO);
   rtlreg_t addr = cpu.stvec;
-  //rtl_jr(&addr);
-  cpu.pc = addr;
+  rtl_jr(&addr);
+  //cpu.pc = addr;
 }
 
 bool isa_query_intr(void) {
