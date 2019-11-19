8a667c757f3976f586e1a5237953f9dd899acc9c
tracer-ics2019 <tracer@njuics.org>
2019-11-19 08:12:31 +0800

    >  run 181240019 bright Linux Debian10 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  08:12:31 up 23:02,  1 user,  load average: 0.00, 0.05, 0.04 b87633807bcaedf798ebe9ddd2df8f0a20673fb

--- nexus-am/am/src/riscv32/nemu/cte.c
+++ nexus-am/am/src/riscv32/nemu/cte.c
@@ -6,7 +6,7 @@ static _Context* (*user_handler)(_Event, _Context*) = NULL;
 
 _Context* __am_irq_handle(_Context *c) {
   _Context *next = c;
-  //printf("%d\n",c->cause);
+  printf("%d\n",c->cause);
   if (user_handler) {
     _Event ev = {0};
     switch (c->cause) {	
