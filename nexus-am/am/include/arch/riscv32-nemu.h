ea2f15d57c2f0a7db4b0c4af59398ae759573556
tracer-ics2019 <tracer@njuics.org>
2019-11-25 17:46:50 +0800

    >  run 181240019 bright Linux Debian10 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  17:46:49 up 1 day, 15:17,  1 user,  load average: 0.19, 0.17, 0.07 2f8f4bac28354739e46811577d2b201a7b13f04b

--- nexus-am/am/include/arch/riscv32-nemu.h
+++ nexus-am/am/include/arch/riscv32-nemu.h
@@ -2,10 +2,14 @@
 #define __ARCH_H__
 
 struct _Context {
-  uintptr_t gpr[32];
+  /*
+  uintptr_t gpr[30];
   struct _AddressSpace *as;
   uintptr_t cause, status, epc;
-  //struct _AddressSpace *as;
+  */
+  uintptr_t epc, status, cause;
+  uintptr_t gpr[31];
+  struct _AddressSpace *as;
 };
 
 #define GPR1 gpr[17]
