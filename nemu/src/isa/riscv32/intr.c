06d2bbf2c210e70ae3f49c5967f7ea428b284a5c
tracer-ics2019 <tracer@njuics.org>
2019-11-25 23:39:32 +0800

    >  compile 181240019 bright Linux Debian10 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  23:39:32 up 1 day, 20:30,  5 users,  load average: 0.23, 0.19, 0.08 a88ab60fb8289529e2e6171a3f88bdc3624c25b2

--- nemu/src/isa/riscv32/intr.c
+++ nemu/src/isa/riscv32/intr.c
@@ -5,7 +5,7 @@ void raise_intr(uint32_t NO, vaddr_t epc) {
    * That is, use ``NO'' to index the IDT.
    */
   rtl_li(&cpu.sepc, epc);
-  //rtl_li(&cpu.scause, NO);
+  rtl_li(&cpu.scause, NO);
   rtlreg_t addr = cpu.stvec;
   rtl_jr(&addr);
   printf("scause %x \n",cpu.scause);
