73cad81e87750d569ed4681fed6edf5e9d18e252
tracer-ics2019 <tracer@njuics.org>
2019-11-26 19:18:19 +0800

    >  run 181240019 bright Linux Debian10 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  19:18:19 up 2 days,  4:17,  1 user,  load average: 0.00, 0.00, 0.00 171d4b74812cbfd8ef60d33a5dbb89730be8dd3

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -3,7 +3,7 @@
 void sys_exit(_Context *c);
 void sys_yield(_Context *c);
 void sys_brk(_Context *c);
-
+void sys_write(_Context *c);
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
@@ -17,6 +17,9 @@ _Context* do_syscall(_Context *c) {
 		  break;
 	  case SYS_brk :
 		  sys_brk(c);
+		  break;
+	  case SYS_write :
+		  sys_write(c);
 		  break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
@@ -37,4 +40,19 @@ void sys_yield(_Context *c){
 void sys_brk(_Context *c){
   c->GPRx = 0;
   return;
+}
+
+void sys_write(_Context *c){
+  int fd = c->GPR2;
+  char *ptr = (char*)(c->GPR3);
+  uint32_t count = c->GPR4;
+  if(fd == 1 || fd == 2){
+    for(int i=0; i<count; i++){
+	  _putc(ptr[i]);
+	  c->GPRx = i;
+	}
+  }else{
+    c->GPRx = -1;
+	return;
+  }
 }
