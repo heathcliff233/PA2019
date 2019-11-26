a027a14870784475be8c1f88d1121ad9ebb3f146
tracer-ics2019 <tracer@njuics.org>
2019-11-26 16:41:38 +0800

    >  run 181240019 bright Linux Debian10 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  16:41:38 up 2 days,  2:35,  1 user,  load average: 0.05, 0.03, 0.00 169ec4a9413d5325e250ec42158e38fc51687c8

--- navy-apps/tests/dummy/dummy.c
+++ navy-apps/tests/dummy/dummy.c
@@ -1,11 +1,8 @@
 #include <stdint.h>
-#include <assert.h>
 
 #define SYS_yield 1
 extern int _syscall_(int, uintptr_t, uintptr_t, uintptr_t);
 
 int main() {
-  int ret =_syscall_(SYS_yield, 0, 0, 0);
-  assert(0);
-  return ret;
+  return _syscall_(SYS_yield, 0, 0, 0);
 }
