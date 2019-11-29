70ecf61d05d5fbc7b1ec41ce04aadfc447d2ba21
tracer-ics2019 <tracer@njuics.org>
2019-11-29 17:32:07 +0800

    >  run 181240019 bright Linux Debian10 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  17:32:07 up 2 days, 15:03,  1 user,  load average: 0.25, 0.12, 0.10 f920f1fdadd85d2f20128c681ffe3d11ddfa894

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,9 +1,16 @@
 #include "common.h"
 #include "syscall.h"
+#include "fs.h"
+
 void sys_exit(_Context *c);
 void sys_yield(_Context *c);
-void sys_brk(_Context *c);
 void sys_write(_Context *c);
+void sys_brk(_Context *c);
+void sys_open(_Context *c);
+void sys_close(_Context *c);
+void sys_read(_Context *c);
+void sys_lseek(_Context *c);
+
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
@@ -17,13 +24,29 @@ _Context* do_syscall(_Context *c) {
 		  printf("sys_yield\n");
 		  sys_yield(c);
 		  break;
-	  case SYS_brk :
-		  printf("sys_brk\n");
-		  sys_brk(c);
+	  case SYS_open :
+		  printf("sys_open\n");
+		  sys_open(c);
+		  break;
+	  case SYS_close :
+		  printf("sys_close\n");
+		  sys_close(c);
 		  break;
 	  case SYS_write :
 		  printf("sys_write\n");
 		  sys_write(c);
+		  break;
+	  case SYS_read :
+		  printf("sys_read\n");
+		  sys_read(c);
+		  break;
+	  case SYS_lseek :
+		  printf("sys_lseek\n");
+		  sys_lseek(c);
+		  break;
+	  case SYS_brk :
+		  printf("sys_break\n");
+		  sys_brk(c);
 		  break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
@@ -41,6 +64,7 @@ void sys_yield(_Context *c){
   //do_syscall(c);
 }
 
+/*
 void sys_brk(_Context *c){
   c->GPRx = 0;
   return;
@@ -61,4 +85,31 @@ void sys_write(_Context *c){
   }
   return;
   
+}
+*/
+
+void sys_write(_Context* c){
+  c->GPRx = fs_write(c->GPR2, (char*)c->GPR3, c->GPR4);
+  return;
+}
+
+void sys_brk(_Context* c){
+  c->GPRx = 0;
+  return;
+}
+
+void sys_open(_Context* c){
+  c->GPRx = fs_open((char*)c->GPR2, 0, 0);
+}
+
+void sys_close(_Context* c){
+  c->GPRx = fs_close(c->GPR2);
+}
+
+void sys_read(_Context* c){
+  c->GPRx = fs_read(c->GPR2, (char*)c->GPR3, c->GPR4);
+}
+
+void sys_lseek(_Context* c){
+  c->GPRx = fs_lseek(c->GPR2, c->GPR3, c->GPR4);
 }
