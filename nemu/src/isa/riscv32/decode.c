3d71814ca3a72f0f0a6ad51b01d42a77bb813a8a
tracer-ics2019 <tracer@njuics.org>
2019-12-06 14:40:52 +0800

    >  compile 181240019 bright Linux Debian10 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  14:40:52 up 3 days,  5:40,  1 user,  load average: 0.00, 0.02, 0.02 a62fb5e69f10e8586736c3253c0f98f28d4ecc7d

--- nemu/src/isa/riscv32/decode.c
+++ nemu/src/isa/riscv32/decode.c
@@ -83,7 +83,7 @@ make_DHelper(st) {
 //void raise_intr(uint32_t NO, vaddr_t epc);
 make_DHelper(EC){
   decode_op_r(id_src, decinfo.isa.instr.rs1, true);
-  decode_op_i(id_src2, decinfo.isa.instr.simm11_0, true);
+  decode_op_i(id_src2, decinfo.isa.instr.csr, true);
   print_Dop(id_src->str, OP_STR_SIZE, "%d(%s)", id_src2->val, reg_name(id_src->reg, 4));
   decode_op_r(id_dest, decinfo.isa.instr.rd, false);
 
--- nemu/src/isa/riscv32/diff-test.c
+++ nemu/src/isa/riscv32/diff-test.c
@@ -9,10 +9,6 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
 		flag = false;
 	}
   }
-  if(cpu.stvec != (*ref_r).stvec){
-    printf("stvec is %x but should get %x\n", cpu.stvec, (*ref_r).stvec);
-    flag = true;
-  }
 
   if(cpu.pc != (*ref_r).pc){
     printf("pc is %x but should be %x\n", cpu.pc, (*ref_r).pc);
