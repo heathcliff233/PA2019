f8395e1bded07bc444107b9de1b25544a8476b19
tracer-ics2019 <tracer@njuics.org>
2019-12-15 23:38:51 +0800

    >  compile 181240019 bright Linux Debian10 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  23:38:51 up 3 days, 20:27,  1 user,  load average: 0.43, 0.20, 0.12 fa7d7e3d1971169ad297fe9a3efa448d1f4aee3

--- nemu/src/isa/riscv32/exec/ldst.c
+++ nemu/src/isa/riscv32/exec/ldst.c
@@ -13,10 +13,13 @@ make_EHelper(ld) {
   }
 }
 make_EHelper(lh) {
-     rtl_lm(&reg_l(id_dest->reg), &id_src->addr, decinfo.width);
-     rtl_sext(&reg_l(id_dest->reg), &reg_l(id_dest->reg), decinfo.width); 
 	 printf("lw ra: %x\n dest: %d\n",cpu.gpr[1]._32,id_dest->reg);
 	 printf("pc %x\n",cpu.pc);
+
+     rtl_lm(&reg_l(id_dest->reg), &id_src->addr, decinfo.width);
+     rtl_sext(&reg_l(id_dest->reg), &reg_l(id_dest->reg), decinfo.width); 
+	 //printf("lw ra: %x\n dest: %d\n",cpu.gpr[1]._32,id_dest->reg);
+	 //printf("pc %x\n",cpu.pc);
 }
 make_EHelper(st) {
   rtl_sm(&id_src->addr, &id_dest->val, decinfo.width);
