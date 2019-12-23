03c4ff6c3fba151f7ba264880b9e9cc5254d95a5
tracer-ics2019 <tracer@njuics.org>
2019-12-23 21:05:32 +0800

    >  run 181240019 bright Linux Debian10 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  21:05:32 up 4 days,  5:37,  1 user,  load average: 0.31, 0.35, 0.17 4e5a1e00e9e08ab5d73a4164ba45a7ea9b58f684

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -20,13 +20,13 @@ void hello_fun(void *arg) {
 }
 
 void init_proc() {
-  /*
+  
   context_kload(&pcb[0], (void *)hello_fun);
-  context_uload(&pcb[1], "/bin/init");
+  //context_uload(&pcb[1], "/bin/init");
   switch_boot_pcb();
-  */
-  Log("Initializing processes...");
-  naive_uload(NULL, "/bin/init");
+  
+  //Log("Initializing processes...");
+  //naive_uload(NULL, "/bin/init");
   // load program here
 
 }
@@ -36,7 +36,7 @@ _Context* schedule(_Context *prev) {
   current->cp = prev;
 
   // always select pcb[0] as the new process
-  current = (current==&pcb[0] ? &pcb[1] : &pcb[0]);
+  //current = (current==&pcb[0] ? &pcb[1] : &pcb[0]);
 
   // then return the new context
   return current->cp;
